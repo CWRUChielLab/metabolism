9ed447d1ec3419ec146fa3fcf4ddbe79b49ee334
Jeff Gill <jeff.gill@case.edu>
2010-01-27 10:31:04 -0500

    Further improvements to analysis.R

--- src/analysis.R
+++ src/analysis.R
@@ -12,15 +12,34 @@
 
 # Import command line arguments
 Args = commandArgs()
-batch = Args[6]
-experiments = Args[7]
-iters = Args[8]
-x = Args[9]
-y = Args[10]
-atoms = Args[11]
+batch = as.character(Args[6])
+experiments = as.integer(Args[7])
+iters = as.integer(Args[8])
+x = as.integer(Args[9])
+y = as.integer(Args[10])
+atoms = as.integer(Args[11])
 
 # Change the working directory to the batch directory
 setwd(paste("data/batch/", batch, sep=""))
+
+# The diffusion coefficient and expected standard deviation
+d = 3/8
+expected.sd = sqrt(2*d*iters)
+
+# Define the inverse function of
+# D_alpha = sqrt(-log(alpha/2)/(2*n))
+# for the Kolmogorov-Smirnov test
+minalpha = function(D,n) 2*exp(-2*n*D^2)
+
+# Suggested number of bins in each histogram
+bins = 30
+
+# Colors 
+green = "#249800"
+darkgreen = "#237A09"
+blue = "#001055"
+lightblue = "#0020AB"
+red = "#FF0000"
 
 for (i in 1:experiments)
 {
@@ -32,15 +51,28 @@ for (i in 1:experiments)
    # Draw histograms
    pdf(file = paste("histograms_", batch, "_", this.experiment, ".pdf", sep=""))
    par(mfrow = c(2,2))
-   hist(diff.data$dx_actual, main="Net Horizontal Displacement (Actual)", xlab="Displacement", ylab="Number of Atoms", breaks=25, col=rgb(0,0.75,0.15))
-   hist(diff.data$dx_ideal,  main="Net Horizontal Displacement (Ideal)",  xlab="Displacement", ylab="Number of Atoms", breaks=25, col=rgb(0,0.5,0.1))
-   hist(diff.data$dy_actual, main="Net Vertical Displacement (Actual)",   xlab="Displacement", ylab="Number of Atoms", breaks=25, col=rgb(0,0.15,0.75))
-   hist(diff.data$dy_ideal,  main="Net Vertical Displacement (Ideal)",    xlab="Displacement", ylab="Number of Atoms", breaks=25, col=rgb(0,0.1,0.5))
+   hist(diff.data$dx_actual, main="Net Horizontal Displacement (Actual)", xlab="Displacement", breaks=bins, col=green,     freq=FALSE)
+   curve(dnorm(x, mean=0, sd=expected.sd), lwd=2, col=red, add=TRUE)
+   hist(diff.data$dx_ideal,  main="Net Horizontal Displacement (Ideal)",  xlab="Displacement", breaks=bins, col=darkgreen, freq=FALSE)
+   curve(dnorm(x, mean=0, sd=expected.sd), lwd=2, col=red, add=TRUE)
+   hist(diff.data$dy_actual, main="Net Vertical Displacement (Actual)",   xlab="Displacement", breaks=bins, col=lightblue, freq=FALSE)
+   curve(dnorm(x, mean=0, sd=expected.sd), lwd=2, col=red, add=TRUE)
+   hist(diff.data$dy_ideal,  main="Net Vertical Displacement (Ideal)",    xlab="Displacement", breaks=bins, col=blue,      freq=FALSE)
+   curve(dnorm(x, mean=0, sd=expected.sd), lwd=2, col=red, add=TRUE)
 
-   #hist(diff.data$dx_actual, main="Net Horizontal Displacement (Actual)", xlab="Displacement", freq=FALSE)
-   #hist(diff.data$dx_ideal,  main="Net Horizontal Displacement (Ideal)",  xlab="Displacement", freq=FALSE)
-   #hist(diff.data$dy_actual, main="Net Vertical Displacement (Actual)",   xlab="Displacement", freq=FALSE)
-   #hist(diff.data$dy_ideal,  main="Net Vertical Displacement (Ideal)",    xlab="Displacement", freq=FALSE)
+   # Draw CDFs
+   par(mfrow = c(2,2))
+   plot(ecdf(diff.data$dx_actual), lwd=1, verticals=TRUE, do.points=FALSE, col=green)
+   curve(pnorm(x, mean=0, sd=expected.sd), lwd=2, lty=2, col=red, add=TRUE)
+   ks = ks.test(diff.data$dx_actual, "pnorm", mean=0, sd=expected.sd)
+   text(200, 0.2, paste("D = ", format(ks$statistic, digits=3), sep=""))
+   #text(200, 0,1, paste("minalpha = ", format(minalpha(ks$statistic, atoms), digits=3), sep=""))
+   plot(ecdf(diff.data$dx_ideal),  lwd=1, verticals=TRUE, do.points=FALSE, col=darkgreen)
+   curve(pnorm(x, mean=0, sd=expected.sd), lwd=2, lty=2, col=red, add=TRUE)
+   plot(ecdf(diff.data$dy_actual), lwd=1, verticals=TRUE, do.points=FALSE, col=lightblue)
+   curve(pnorm(x, mean=0, sd=expected.sd), lwd=2, lty=2, col=red, add=TRUE)
+   plot(ecdf(diff.data$dy_ideal),  lwd=1, verticals=TRUE, do.points=FALSE, col=blue)
+   curve(pnorm(x, mean=0, sd=expected.sd), lwd=2, lty=2, col=red, add=TRUE)
 
    dev.off()
    setwd("..")
