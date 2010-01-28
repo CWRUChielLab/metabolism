543a9c18ba3068a8f90cb3f677677025a2bb9190
Jeff Gill <jeff.gill@case.edu>
2010-01-27 23:56:40 -0500

    Further improvements to analysis.R

--- src/analysis.R
+++ src/analysis.R
@@ -24,12 +24,12 @@ setwd(paste("data/batch/", batch, sep=""))
 
 # The diffusion coefficient and expected standard deviation
 d = 3/8
-expected.sd = sqrt(2*d*iters)
+expected_sd = sqrt(2*d*iters)
 
 # Define the inverse function of
 # D_alpha = sqrt(-log(alpha/2)/(2*n))
 # for the Kolmogorov-Smirnov test
-minalpha = function(D,n) 2*exp(-2*n*D^2)
+min_alpha = function(D,n) 2*exp(-2*n*D^2)
 
 # Suggested number of bins in each histogram
 bins = 30
@@ -41,43 +41,68 @@ blue = "#001055"
 lightblue = "#0020AB"
 red = "#FF0000"
 
+# Iterate over all experiments in the batch
 for (i in 1:experiments)
 {
-   # Import data for experiment i
-   this.experiment = formatC(i, flag="0", width=nchar(experiments))
-   setwd(this.experiment)
-   diff.data = read.table(paste("diffusion.out.", this.experiment, sep=""), header=TRUE)
+   # Change the working directory and import data for the currect experiment
+   this_experiment = formatC(i, flag="0", width=nchar(experiments))
+   setwd(this_experiment)
+   diffusion_data = read.table(paste("diffusion.out.", this_experiment, sep=""), header=TRUE)
+
+   # Open a PDF graphics device and set a few parameters
+   pdf(file = paste("analysis_", batch, "_", this_experiment, ".pdf", sep=""), family="Palatino")
+   par(mfrow = c(2,2))
+   par(font.lab=2)
+   #par(font.axis=2)
+   #par(font=3)
 
    # Draw histograms
-   pdf(file = paste("histograms_", batch, "_", this.experiment, ".pdf", sep=""))
-   par(mfrow = c(2,2))
-   hist(diff.data$dx_actual, main="Net Horizontal Displacement (Actual)", xlab="Displacement", breaks=bins, col=green,     freq=FALSE)
-   curve(dnorm(x, mean=0, sd=expected.sd), lwd=2, col=red, add=TRUE)
-   hist(diff.data$dx_ideal,  main="Net Horizontal Displacement (Ideal)",  xlab="Displacement", breaks=bins, col=darkgreen, freq=FALSE)
-   curve(dnorm(x, mean=0, sd=expected.sd), lwd=2, col=red, add=TRUE)
-   hist(diff.data$dy_actual, main="Net Vertical Displacement (Actual)",   xlab="Displacement", breaks=bins, col=lightblue, freq=FALSE)
-   curve(dnorm(x, mean=0, sd=expected.sd), lwd=2, col=red, add=TRUE)
-   hist(diff.data$dy_ideal,  main="Net Vertical Displacement (Ideal)",    xlab="Displacement", breaks=bins, col=blue,      freq=FALSE)
-   curve(dnorm(x, mean=0, sd=expected.sd), lwd=2, col=red, add=TRUE)
+   current_data = cbind(diffusion_data$dx_actual,
+                        diffusion_data$dx_ideal,
+                        diffusion_data$dy_actual,
+                        diffusion_data$dy_ideal)
+   plot_title = c("Net Horizontal Displacement (Actual)",
+                  "Net Horizontal Displacement (Ideal)",
+                  "Net Vertical Displacement (Actual)",
+                  "Net Vertical Displacement (Ideal)")
+   plot_color = c(green,
+                  darkgreen,
+                  lightblue,
+                  blue)
+   for (j in 1:4)
+   {
+      hist(current_data[,j], main=plot_title[j], xlab="Displacement", breaks=bins, col=plot_color[j], freq=FALSE)
+      curve(dnorm(x, mean=0, sd=expected_sd), lwd=2, col=red, add=TRUE)
+   }
 
-   # Draw CDFs
-   par(mfrow = c(2,2))
-   plot(ecdf(diff.data$dx_actual), lwd=1, verticals=TRUE, do.points=FALSE, col=green)
-   curve(pnorm(x, mean=0, sd=expected.sd), lwd=2, lty=2, col=red, add=TRUE)
-   ks = ks.test(diff.data$dx_actual, "pnorm", mean=0, sd=expected.sd)
-   text(200, 0.2, paste("D = ", format(ks$statistic, digits=3), sep=""))
-   #text(200, 0,1, paste("minalpha = ", format(minalpha(ks$statistic, atoms), digits=3), sep=""))
-   plot(ecdf(diff.data$dx_ideal),  lwd=1, verticals=TRUE, do.points=FALSE, col=darkgreen)
-   curve(pnorm(x, mean=0, sd=expected.sd), lwd=2, lty=2, col=red, add=TRUE)
-   plot(ecdf(diff.data$dy_actual), lwd=1, verticals=TRUE, do.points=FALSE, col=lightblue)
-   curve(pnorm(x, mean=0, sd=expected.sd), lwd=2, lty=2, col=red, add=TRUE)
-   plot(ecdf(diff.data$dy_ideal),  lwd=1, verticals=TRUE, do.points=FALSE, col=blue)
-   curve(pnorm(x, mean=0, sd=expected.sd), lwd=2, lty=2, col=red, add=TRUE)
+   # Draw CDFs and conduct Kolmogorov-Smirnov test
+   current_data = cbind(diffusion_data$dx_actual,
+                        diffusion_data$dx_ideal,
+                        diffusion_data$dy_actual,
+                        diffusion_data$dy_ideal)
+   plot_title = c("Net Horizontal Displacement (Actual)",
+                  "Net Horizontal Displacement (Ideal)",
+                  "Net Vertical Displacement (Actual)",
+                  "Net Vertical Displacement (Ideal)")
+   plot_color = c(green,
+                  darkgreen,
+                  lightblue,
+                  blue)
+   for (j in 1:4)
+   {
+      plot(ecdf(current_data[,j]), main=plot_title[j], xlab="Displacement", ylab="Cumulative Density", lwd=1, verticals=TRUE, do.points=FALSE, col=plot_color[j])
+      curve(pnorm(x, mean=0, sd=expected_sd), lwd=2, lty=2, col=red, add=TRUE)
+      ks_results = suppressWarnings(ks.test(current_data[,j], "pnorm", mean=0, sd=expected_sd))
+      min_value = min(current_data[,j])
+      max_value = max(current_data[,j])
+      data_range = max_value-min_value
+      text(min_value+0.8*data_range, 0.3, paste("D = ", format(ks_results$statistic, digits=3), sep=""))
+      text(min_value+0.8*data_range, 0.2, paste("p = ", format(ks_results$p.value, digits=3), sep=""))
+      text(min_value+0.8*data_range, 0.1, bquote(alpha[min] ~ " = " ~ .(format(min_alpha(ks_results$statistic, atoms), digits=3))))
+   }
 
+   # Close the PDF graphics device and reset the working directory
    dev.off()
    setwd("..")
-
-   # ks = ks.test(x, "pnorm", mean=0, sd=1)
-   # ks$statistic
 }
 
